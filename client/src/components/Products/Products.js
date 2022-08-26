cc75eb803c94150a419c4cb0e3941d549341a8c8
Moataz Rabeea <moatazrabeea@gmail.com>
2022-08-26 18:24:51 +0200

    adding react-modal

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React,{useState} from "react";
 import  '../../css/Products/Products.css'
+import ProductModal from "./ProductModal";
+
  function Products(props) {
- 
+  const[product,setProduct] = useState('');
+
+  const openModal = (product)=>{
+    setProduct(product)
+  }
+
+  const closeModal = ()=>{
+    setProduct(false)
+  }
+
   return (
     <div className="product-wrapper">
         {props.products.map(product => (
           <div key={product.id}>    
             <div className='product-item'>
+            <a href="#" onClick={()=>openModal(product)}>
                 <img src={product.imageUrl} alt={product.title}></img>
+              </a>
                 <div className='product-desc'>
                     <p>{product.title}</p>
                     <span>{product.price}</span>
@@ -16,6 +29,10 @@ import  '../../css/Products/Products.css'
             </div>
           </div>  
         ))}
+
+        <ProductModal product={product} closeModal={closeModal}>
+
+        </ProductModal>
     </div>
   )
 }
