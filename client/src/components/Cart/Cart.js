70af1f273a6862f734f242dfb0374ea63a8470e1
Moataz Rabeea <moatazrabeea@gmail.com>
2022-11-07 23:28:57 +0200

    order component

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -4,14 +4,20 @@ import Checkout from '../Checkout/Checkout';
 import Bounce from 'react-reveal/Bounce'
 import { connect } from 'react-redux';
 import { removeCart } from '../../actions/cart';
+import Modal from 'react-modal';
  function Cart(props) {
  const[showForm,setShowForm] = useState(false)
 
  const[value,setValue] = useState("");
+ const[order,setOrder] = useState(false);
 
  const handleChange =(e) => {
   
      setValue((prevState) => ({...prevState,[e.target.name]:e.target.value}))
+ }
+
+ const closeModal =()=>{
+  setOrder(false)
  }
 
  const submitOrder =(e) => {
@@ -20,7 +26,7 @@ import { removeCart } from '../../actions/cart';
      name:value.name,
      email:value.email
     } 
-    console.log(order)
+    setOrder(order)
 }
   return (
     
@@ -28,6 +34,43 @@ import { removeCart } from '../../actions/cart';
      <div className='cart-title'> {props.cartItems.length === 0 ? 'Cart Empty' : <p>
       There is {props.cartItems.length} products in cart
       </p>}</div>
+      {/* Modal */}
+      <Modal isOpen={order} onRequestClose={closeModal}>
+        <div className='order-info'>
+          <span className='close-icon' onClick={closeModal}>&times;</span>
+          <p className='alert-success'> order done successfully</p>
+          <table>
+            <tr>
+              <td>Name:</td>
+              <td>{order.name}</td>
+            </tr>
+
+            <tr>
+              <td>Email:</td>
+              <td>{order.email}</td>
+            </tr>
+
+            <tr>
+              <td>Total:</td>
+              <td>{props.cartItems.reduce((a, p)=>{
+                    return a + p.price
+              },0)}</td>
+            </tr>
+
+            <tr>
+              <td>Selected Items:</td>
+              <td>{props.cartItems.map(p => (
+                <div className='cart-data'>
+                 <p>Number of this product: {p.qty}</p>
+                 <p>Title of this product: {p.title}</p>
+                </div>
+              ))}</td>
+              
+            </tr>
+
+          </table>
+        </div>
+      </Modal>
       <Bounce bottom>
       <div className='cart-itmes'>
         {props.cartItems.map(item =>(
